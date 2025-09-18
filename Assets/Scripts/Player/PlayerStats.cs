96ad7357349425fb8c9bbac3e40f9a2d041bf192
Mau <mauvepinktree@gmail.com>
2025-09-18 10:50:12 -0300

    add PlayerStats class

---
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerStats {
+    public float maxHealth;
+    public float defense;
+    public float attackDamage;
+    public float attackRate;
+    public float attackSize;
+    public float movementSpeed;
+    public float luck;
+    public float experienceGainRate;
+    public float moneyGainRate;
+}
