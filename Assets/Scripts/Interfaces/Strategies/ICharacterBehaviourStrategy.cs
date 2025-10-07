04e455ee9e67f50fa9f0d006a557bedebcf7f47e
Mau <mauvepinktree@gmail.com>
2025-10-07 01:18:38 -0300

    add basic enemy strategy

---
+++ Assets/Scripts/Interfaces/Strategies/ICharacterBehaviourStrategy.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public interface ICharacterBehaviourStrategy {
+    /// <summary>
+    /// Used to draw gizmos for the AI.
+    /// </summary>
+    public float GetComforRadius();
+    /// <summary>
+    /// Used to draw gizmos for the AI.
+    /// </summary>
+    /// <returns></returns>
+    public float GetAwarenessRadius();
+    /// <summary>
+    /// Handles the movement behaviour of the AI, put this inside a FixedUpdate block.
+    /// </summary>
+    public void HandleMovement(Transform transform, Rigidbody2D rb, Transform targetTransform, float movementSpeed, Vector2 pushVelocity);
+}
